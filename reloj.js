4c5d42e3c109bed87a75ac8a80dd91d91e20863d
Juan Ignacio Gil Palacios <jgilpalacios@users.noreply.github.com>
2016-04-10 23:41:11 +0200

    compactado

--- reloj.js
+++ reloj.js
@@ -1,30 +1,32 @@
-{
-var tinicial;
-var parar=0;
+var t=0;
+var timer;
+var activo=false;
 var destino;
-function tiempo(){
-	var ahora=new Date();
-	var tahora=ahora.getHours()*3600+
-	ahora.getMinutes()*60+ahora.getSeconds()+
-	ahora.getMilliseconds()/1000;
-	return tahora-tinicial;
-}
-function set(destino){
-	this.destino=destino;
-}
 function reloj(){
-	var ahora=new Date();
-	var tahora=ahora.getHours()*3600+
-	ahora.getMinutes()*60+ahora.getSeconds()+
-	ahora.getMilliseconds()/1000;
-	var tt=Math.floor((tahora-tinicial)*100);
-	if((tt%5)==1){
-		var texto=''+tt/100;
-		texto=texto.substring(0,texto.length-1)+' s';
-		document.getElementById(this.destino).innerHTML=texto;
+	if(!activo){
+	   timer=setInterval( muestraT
+		/*function(){
+			t++;
+			var sec=t%60;
+			var minut=((t-sec)/60)%60;
+			var hor=(t-sec-minut*60)/3600;
+			var tt=ajusta_der('00', ''+hor)+':'+ajusta_der('00', ''+minut)+':'+ajusta_der('00', ''+sec);
+			document.getElementById(destino).innerHTML=tt;
+		}*/
+	   , 1000);
+	   activo=true;
 	}
-	if(parar==0) setTimeout("reloj()",10);
 }
+
+function muestraT(){
+	var sec=t%60;
+	var minut=((t-sec)/60)%60;
+	var hor=(t-sec-minut*60)/3600;
+	var tt=ajusta_der('00', ''+hor)+':'+ajusta_der('00', ''+minut)+':'+ajusta_der('00', ''+sec);
+	document.getElementById(destino).innerHTML=tt;
+	t++;
+}
+
 function ajusta_der(patron, texto){
 	var car=texto.length;
 	var car2=patron.length;
@@ -35,34 +37,8 @@ function ajusta_der(patron, texto){
 	}
 	return ''+res;
 }
+
 function para(){
-	this.parar=1;
-}
-function sigue(){
-	this.parar=0;
-	reloj();
-}
-function sigue_c(){
-	this.parar=0;
-	c_reloj();
-}
-function c_reloj(){
-	var ahora=new Date();
-	var tahora=ahora.getHours()*3600+
-	ahora.getMinutes()*60+ahora.getSeconds()+
-	ahora.getMilliseconds()/1000;
-	var tt=Math.floor(tahora-tinicial);
-	var thn=Math.floor(tt/3600);
-	var tmn=Math.floor((tt%3600)/60);
-	var tsn=tt%60;
-	var texto=''+ajusta_der('00', thn+'')+':'+ajusta_der('00', tmn+'')+':'+ajusta_der('00', tsn+'');
-	document.getElementById(this.destino).innerHTML=texto;
-	if(parar==0)setTimeout("c_reloj()",990);//se refresca cada 990 ms
-}
-function tinicial(){
-	var ahora=new Date();
-	this.tinicial=ahora.getHours()*3600+
-	ahora.getMinutes()*60+ahora.getSeconds()+
-	ahora.getMilliseconds()/1000;
-} 
+	clearInterval(timer);
+	activo=false;
 }
